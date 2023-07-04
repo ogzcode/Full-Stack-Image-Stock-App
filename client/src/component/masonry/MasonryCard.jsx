3b2c16706c3e067b0c61fe8a039404d209497673
ogzcode <oguzguc44@gmail.com>
2023-07-04 09:08:49 +0300

    Added confirmation modal to delete

--- client/src/component/masonry/MasonryCard.jsx
+++ client/src/component/masonry/MasonryCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { deleteImage } from "../../auth/core/request";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
@@ -8,6 +9,7 @@ import Tags from "./Tags";
 
 
 function MasonryCard({ data }) {
+    const [show, setShow] = useState(false);
     const handleDelete = async () => {
         try {
             await deleteImage(data._id, data.name);
@@ -18,7 +20,7 @@ function MasonryCard({ data }) {
         }
     }
     return (
-        <div className="masonary relative border-[4px] border-white overflow-hidden rounded transition">
+        <div className="masonary relative border-[8px] border-white overflow-hidden rounded transition">
             <LazyLoadImage src={`http://localhost:3000/${data.name}`}
                 alt="Image Alt"
                 className="w-full h-full object-fill"
@@ -27,14 +29,24 @@ function MasonryCard({ data }) {
             <div className="masonary-bottom absolute bottom-0 left-0 w-full bg-black/60 py-[8px] px-[12px]">
                 <h4 className="text-[16px] text-white uppercase">{data.title}</h4>
             </div>
-            <div className="masonary-hover opacity-0 invisible absolute top-0 left-0 bg-black/90 w-full h-full text-center">
+            <div className="masonary-hover flex justify-center items-center opacity-0 invisible absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black/90 w-full h-full text-center"
+                onMouseLeave={() => setShow(false)}
+            >
+
                 <div className="inside text-white pt-[2rem] lg:pt-[4rem]">
-                    <p className="text-center mb-4 cursor-pointer" onClick={() => handleDelete()}>
-                        <FaTrash className="text-[36px] border-white border-[1px] text-white rounded mx-auto p-2 transition hover:text-black hover:bg-white" />
+                    <p className="text-center mb-4 cursor-pointer absolute top-4 right-4" onClick={() => setShow(!show)}>
+                        <FaTrash className="text-[36px] border-white border-[1px] text-white rounded mx-auto p-2 transition hover:text-red-500 hover:bg-white" />
                     </p>
-                    <h4 className="text-[16px] uppercase font-semibold">{data.title}</h4>
+                    <h4 className="text-[24px] uppercase font-semibold">{data.title}</h4>
                     <span className="text-[12px] font-light">{data.time}</span>
                     <Tags tags={data.tags} />
+                </div>
+                <div className={`${!show && 'hidden'} bg-white absolute top-2 left-1/2 -translate-x-1/2 px-4 py-2 rounded`}>
+                    <p className="text-sm text-slate-800">Are you sure want to delete ?</p>
+                    <div className="flex justify-end mt-2">
+                        <button className="px-4 py-1 text-sm bg-slate-200 text-slate-700 rounded" onClick={() => setShow(false)}>No</button>
+                        <button className="px-4 py-1 text-sm bg-red-200 text-red-700 rounded ml-2" onClick={() => handleDelete()}>Yes</button>
+                    </div>
                 </div>
             </div>
         </div>
