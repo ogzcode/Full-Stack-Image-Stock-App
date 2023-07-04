c5ef549e7a1689bceb2f03d74178b99e2eb80b3b
ogzcode <oguzguc44@gmail.com>
2023-07-04 09:08:03 +0300

    Photo add modal made full-screen

--- client/src/component/core/AddImageForm.jsx
+++ client/src/component/core/AddImageForm.jsx
@@ -15,8 +15,6 @@ export default function AddImageForm({ onChange }) {
         e.preventDefault();
 
         let res = await uploadImage(title, image, tags);
-        console.log(res);
-
 
         setTitle("");
         setImage("");
@@ -32,49 +30,50 @@ export default function AddImageForm({ onChange }) {
         }
     };
     return (
-        <div className="absolute z-10 top-0 left-1/2 rounded-lg -translate-x-1/2 bg-[#fff] px-16 py-8 z-10 max-w-md w-1/2 "       
-        >
-            <h1 className="text-2xl font-medium mb-8">Add Image</h1>
-            <BsXCircle className="absolute right-2 top-2 text-2xl text-red-500 cursor-pointer"
-                onClick={() => onChange(false)} />
-            <form onSubmit={handleSubmit} onKeyDown={handleKey}>
+        <div className="w-screen h-full flex justify-center items-start bg-gray-900/50 absolute top-0 left-0 z-20">
+            <div className="rounded-lg bg-white px-16 py-8 max-w-md w-1/2 relative mt-60">
+                <h1 className="text-2xl font-medium mb-8">Add Image</h1>
+                <BsXCircle className="absolute right-2 top-2 text-2xl text-red-500 cursor-pointer"
+                    onClick={() => onChange(false)} />
+                <form onSubmit={handleSubmit} onKeyDown={handleKey}>
 
-                <div className="mb-4">
-                    <label className="block text-left text-sm font-medium leading-6 text-gray-900">
-                        Image Title
-                    </label>
-                    <div className="mt-2">
-                        <input
-                            value={title}
-                            onChange={e => setTitle(e.target.value)}
-                            id="title"
-                            name="title"
-                            type="text"
-                            placeholder="Enter your image title"
-                            className="block w-full outline-0 rounded-md border border-gray-500 py-1 px-2 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                    <div className="mb-4">
+                        <label className="block text-left text-sm font-medium leading-6 text-gray-900">
+                            Image Title
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                value={title}
+                                onChange={e => setTitle(e.target.value)}
+                                id="title"
+                                name="title"
+                                type="text"
+                                placeholder="Enter your image title"
+                                className="block w-full outline-0 rounded border border-gray-500 py-1 px-2 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mb-6">
+                        <label className="block text-left text-sm font-medium leading-6 text-gray-900">
+                            Select Image File
+                        </label>
+                        <input className="select-file block w-full p-1 text-sm border border-gray-500 rounded cursor-pointer text-gray-600 focus:outline-none"
+                            type="file" accept="image/*"
+                            name="image"
+                            onChange={e => setImage(e.target.files[0])}
+                            ref={inputRef}
                         />
                     </div>
-                </div>
 
-                <div className="mb-6">
-                    <label className="block text-left text-sm font-medium leading-6 text-gray-900">
-                        Select Image File
-                    </label>
-                    <input className="block w-full p-1 text-sm border border-gray-500 rounded cursor-pointer text-gray-600 focus:outline-none"
-                        type="file" accept="image/*"
-                        name="image"
-                        onChange={e => setImage(e.target.files[0])} 
-                        ref={inputRef}
-                        />
-                </div>
+                    <MultipleTagInput onChangeTag={setTags} tags={tags} />
 
-                <MultipleTagInput onChangeTag={setTags} tags={tags} />
+                    <button className="mt-6 text-gray-500 border border-gray-500 font-semibold px-8 py-1 rounded-md transition hover:scale-95"
+                        type="submit"
+                    >Submit</button>
 
-                <button className="mt-6 text-gray-500 border border-gray-500 font-semibold px-8 py-1 rounded-md transition hover:scale-95"
-                    type="submit"
-                >Submit</button>
-            
-            </form>
+                </form>
+            </div>
         </div>
     );
 }
